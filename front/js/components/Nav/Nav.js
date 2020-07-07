1da9cc29949bb2fbc21003f0f70934c4a8c601cc
Paul <worship@Valleys-MBP.hsd1.co.comcast.net>
2020-07-07 16:19:16 -0600

    Created and finished nav and dashboard js files

---
+++ front/js/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+conts Nav = {props} =>{
+    rerurn (
+    <Nav>
+    <img>IDB Logo</img>
+    <span>
+        <h3>projects</h3>
+        <h3>settings</h3>
+        <h3>logout</h3>
+    </span>
+    </Nav>
+    );
+};
