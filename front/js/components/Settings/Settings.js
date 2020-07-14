e4f6a37c6e1493d5b720493fd7d5ec7bf014c6dd
Noah <noah@centilgroup.com>
2020-07-14 16:04:36 -0600

    passed props to settings and reconfigured to class component

--- front/js/components/Settings/Settings.js
+++ front/js/components/Settings/Settings.js
@@ -2,22 +2,47 @@
  * @file Settings component.
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 
-const Settings = (props) => {
+
+class Settings extends Component {  
+  constructor(props) {
+  super(props)
+  this.state = {
+    teamMembers: []
+  }
+  }
+
+getTeam = () => {
+  console.log(this.props.email)
+  console.log(this.props.project2data)
+}
+
+componentDidMount = () => {
+  this.getTeam()
+}
+
+render() {
   return (
     <div>
-      <img alt="x-out button" />
-      <h2>Settings</h2>}<h3>Projects:</h3>
-      <p>NEED</p>
-      <h3>Team Members:</h3>
-      <p>NEED</p>
-      <h3>Username:</h3>
-      <p>NEED</p>
-      <h3>Email Address:</h3>
-      <p>NEED</p>
+        {/* <img>x</img> */}
+        <h2>Settings</h2>
+        <h3>Projects:</h3>
+        <p>{this.props.project1name}</p>
+        <p>{this.props.project2name}</p>
+        <h3>Team Members:</h3>
+        <p></p>
+        <h3>Username:</h3>
+        <p>{this.props.username}</p>
+        <h3>Email Address:</h3>
+        <p>{this.props.email}</p>
     </div>
   );
 };
 
+}
+
+
+
 export default Settings;
+
