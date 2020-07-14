ce52ce002aeb025c33773432b2edb76a6b73462f
Noah <noah@centilgroup.com>
2020-07-14 16:28:40 -0600

    map teammates onto dom

--- front/js/components/Settings/Settings.js
+++ front/js/components/Settings/Settings.js
@@ -14,8 +14,21 @@ class Settings extends Component {
   }
 
 getTeam = () => {
-  console.log(this.props.email)
-  console.log(this.props.project2data)
+  const team1 = this.props.project1data[0].map((project) => project.assignee)
+  const team2 = this.props.project2data[0].map((project) => project.assignee)
+  const teams = [...team1,...team2]
+  const teamsNoDupes = teams.reduce((a,b) => {
+    if(a.indexOf(b)<0)a.push(b);return a;},[]);
+    const noUnassigned = teamsNoDupes.filter((member) => member !== 'unassigned')
+  this.setState({ teamMembers: noUnassigned })
+}
+
+displayTeamMembers = () => {
+  return this.state.teamMembers.map(member => {
+    return (
+    <p>{member}</p>
+    )
+  })
 }
 
 componentDidMount = () => {
@@ -31,7 +44,7 @@ render() {
         <p>{this.props.project1name}</p>
         <p>{this.props.project2name}</p>
         <h3>Team Members:</h3>
-        <p></p>
+        {this.displayTeamMembers()}
         <h3>Username:</h3>
         <p>{this.props.username}</p>
         <h3>Email Address:</h3>
