c0f1aa22117f17c281e3a1146f86191d9ccbad2f
parkeeeeeeeeer <epmills@smanet.org>
2020-07-15 12:02:14 -0600

    close time

--- front/js/components/Dashboard/Dashboard.js
+++ front/js/components/Dashboard/Dashboard.js
@@ -20,25 +20,27 @@ export default class Dashboard extends Component {
     //this.calculateLoad();
   };
 
+  calculateVelocity = () => {
+    const featureVelocityCount = this.filterByTypeAndStatus('feature');
+    const riskVelocityCount = this.filterByTypeAndStatus('risk');
+    const debtVelocityCount = this.filterByTypeAndStatus('debt');
+    const defectVelocityCount = this.filterByTypeAndStatus('defect');
+    let answer = [featureVelocityCount, riskVelocityCount, debtVelocityCount, defectVelocityCount];
+    return answer;
+  };
+
+
+  filterByTypeAndStatus = (typeOfWork) => {
+    const info = this.props.projectData[0];
+    const typeFiltered = info.filter((item) => item.type == typeOfWork);
+    const statusFiltered = typeFiltered.filter((item) => item.status == 'backlog' || item.status == 'done');
+    return statusFiltered.length;
+  }
+
   filterTicketsDone = () => {
     const info = this.props.projectData[0];
     const ticketsDone = info.filter((item) => item.status == 'done');
     //this.calculateLoad();
-  };
-
-  calculateWorkTime = () => {  
-    const info = this.props.projectData[0];
-    this.filterTicketsDone();
-    this.filterByType();
-    const totalTime = this.type('endTime' - 'startTime');
-    const averageFetureTotalTime = this.type('totalTime' / 'featureCount');
-    const averageRiskTotalTime = this.type('totalTime' / 'riskCount');
-    const averageDebtTotalTime = this.type('totalTime' / 'debtCount');
-    const averageDefectTotalTime = this.type('totalTime' / 'defectCount');
-    this.calculateLoad();
-    let answer = [averageFeatureTotalTime, averageRiskTotalTime, averageDebtTotalTime, averageDefectTotalTime];
-    return answer;
-    
   };
 
   calculateLoad = () => {
@@ -49,6 +51,21 @@ export default class Dashboard extends Component {
     const defectCount = this.filterByType('defect', info);
     let answer = [featureCount, riskCount, debtCount, defectCount];
     return answer;
+  };
+
+  calculateWorkTime = () => {  
+    const info = this.props.projectData[0];
+    this.filterTicketsDone();
+    this.filterByType();
+    const totalTime = this.type('endTime' - 'startTime');
+    const averageFetureTotalTime = this.type('totalTime' / 'featureCount' *100);
+    const averageRiskTotalTime = this.type('totalTime' / 'riskCount'*100);
+    const averageDebtTotalTime = this.type('totalTime' / 'debtCount'*100);
+    const averageDefectTotalTime = this.type('totalTime' / 'defectCount'*100);
+    this.calculateLoad();
+    let answer = [averageFeatureTotalTime, averageRiskTotalTime, averageDebtTotalTime, averageDefectTotalTime];
+    return answer;
+    
   };
 
 
@@ -66,20 +83,6 @@ export default class Dashboard extends Component {
   };
 
   render() {
-
-    const closeTimeChart = {
-      labels: ['features', 'risks', 'debt', 'defect'],
-      datasets: [
-        {
-          label: 'close Time',
-          backgroundColor: ['#2477B6', '#CAF0F8', '#48B4D9', '#90E1F0'],
-          borderColor: ['#2477B6', '#CAF0F8', '#48B4D9', '#90E1F0'],
-          borderWidth: 2,
-          data: this.calculateWorkTime(),
-        },
-      ],
-    };
-
     const loadChart = {
       labels: ['features', 'risks', 'debt', 'defect'],
       datasets: [
@@ -120,7 +123,11 @@ export default class Dashboard extends Component {
             },
           }}
         />
-        Feature: {this.calculateWorkTime()[0]} Risk: {this.calculateWorkTime()[1]} Debt: {this.calculateWorkTime()[2]} Defect: {this.calculateWorkTime()[3]
+        <h2>Velocity</h2>
+        <h5>Feature: {this.calculateVelocity()[0]} Risk: {this.calculateVelocity()[1]} Debt: {this.calculateVelocity()[2]} Defect: {this.calculateVelocity()[3]}</h5>
+
+        <h2>Close Time</h2>
+        <h5></h5>Feature: {this.calculateWorkTime()[0]} Risk: {this.calculateWorkTime()[1]} Debt: {this.calculateWorkTime()[2]} Defect: {this.calculateWorkTime()[3]</h5>
 
       </section>
     );
