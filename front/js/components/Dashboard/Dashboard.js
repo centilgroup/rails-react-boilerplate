ffeb33e69eedfd9a90d84702697a9c9132b907e8
parkeeeeeeeeer <epmills@smanet.org>
2020-07-15 11:59:26 -0600

    Dashboard chang for close time

--- front/js/components/Dashboard/Dashboard.js
+++ front/js/components/Dashboard/Dashboard.js
@@ -12,15 +12,12 @@ export default class Dashboard extends Component {
       borderColor: [],
       loadData: [],
     };
-  }
+  };
 
-  calculateLoad = () => {
-    const featureCount = this.filterByType('feature');
-    const riskCount = this.filterByType('risk');
-    const debtCount = this.filterByType('debt');
-    const defectCount = this.filterByType('defect');
-    let answer = [featureCount, riskCount, debtCount, defectCount];
-    return answer;
+  filterTicketsDone = () => {
+    const info = this.props.projectData[0];
+    const ticketsDone = info.filter((item) => item.status == 'done');
+    //this.calculateLoad();
   };
 
   calculateVelocity = () => {
@@ -38,14 +35,50 @@ export default class Dashboard extends Component {
     const statusFiltered = typeFiltered.filter((item) => item.status == 'backlog' || item.status == 'done');
     return statusFiltered.length;
   }
- 
-  filterByType = (typeOfWork) => {
+
+  filterTicketsDone = () => {
     const info = this.props.projectData[0];
-    const answer = info.filter((item) => item.type == typeOfWork);
+    const ticketsDone = info.filter((item) => item.status == 'done');
+    //this.calculateLoad();
+  };
+
+  calculateWorkTime = () => {  
+    const info = this.props.projectData[0];
+    this.filterTicketsDone();
+    this.filterByType();
+    const totalTime = this.type('endTime' - 'startTime');
+    const averageFetureTotalTime = this.type('totalTime' / 'featureCount' *100);
+    const averageRiskTotalTime = this.type('totalTime' / 'riskCount'*100);
+    const averageDebtTotalTime = this.type('totalTime' / 'debtCount'*100);
+    const averageDefectTotalTime = this.type('totalTime' / 'defectCount'*100);
+    this.calculateLoad();
+    let answer = [averageFeatureTotalTime, averageRiskTotalTime, averageDebtTotalTime, averageDefectTotalTime];
+    return answer;
+    
+  };
+
+  calculateLoad = () => {
+    const info = this.props.projectData[0];
+    const featureCount = this.filterByType('feature', info);
+    const riskCount = this.filterByType('risk', info);
+    const debtCount = this.filterByType('debt', info);
+    const defectCount = this.filterByType('defect', info);
+    let answer = [featureCount, riskCount, debtCount, defectCount];
+    return answer;
+  };
+
+
+  filterByType = (typeOfWork, tickets) => {
+    const answer = tickets.filter((item) => item.type == typeOfWork);
     return answer.length;
   };
+
   componentDidMount = () => {
     this.calculateLoad();
+
+    this.filterTicketsDone();
+    
+    this.calculateWorkTime();
   };
 
   render() {
@@ -91,6 +124,9 @@ export default class Dashboard extends Component {
         />
         <h2>Velocity</h2>
         <h5>Feature: {this.calculateVelocity()[0]} Risk: {this.calculateVelocity()[1]} Debt: {this.calculateVelocity()[2]} Defect: {this.calculateVelocity()[3]}</h5>
+        <h2>Close Time</h2>
+        <h5></h5>Feature: {this.calculateWorkTime()[0]} Risk: {this.calculateWorkTime()[1]} Debt: {this.calculateWorkTime()[2]} Defect: {this.calculateWorkTime()[3]</h5>
+
       </section>
     );
   }
