94d182a135bb55fa706ea8c7df8e938a09da291e
Daniel Wetteroth <danielcw@gmail.com>
2021-01-21 17:30:18 -0700

    adding digest to production env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -51,7 +52,7 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
-  config.assets.digest = true
+  
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
