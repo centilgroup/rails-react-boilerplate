5dbe9766d5434edad8ef6eaa90643c4999d13797
Daniel Wetteroth <danielcw@gmail.com>
2021-01-25 00:34:07 -0700

    more asset changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,7 +106,7 @@ Rails.application.configure do
     domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: "danielwetteroth2@gmail.com",
+    user_name: "danielwetteroth2",
     password: "DeltaAir90!"
   }
 
