72c0bded31782041bd870c22dedbed6e656bac07
Daniel Wetteroth <danielcw@gmail.com>
2021-06-02 23:35:22 -0600

    fixing issue sync in projects

--- app/services/jira_manager.rb
+++ app/services/jira_manager.rb
@@ -18,7 +18,7 @@ class JiraManager
     }
 
     @user = User.where(id: params[:user_id]).first
-
+    
     @project =
       Project.where(project_id: params[:project_id], user_id: @user.id)
         .order(id: :asc).first
@@ -39,6 +39,9 @@ class JiraManager
     jira_projects = @client.Project.all
     jira_boards = @client.Board.all
 
+
+
+    # binding.pry
     jira_boards.each do |jira_board|
       config = jira_board.configuration
       next if config.try(:location).nil?
@@ -63,35 +66,47 @@ class JiraManager
   end
 
   def sync_issues
+    
     start_at = 0
-    max_results = 100
-    loop do
-      query_options = {
-        start_at: start_at,
-        max_results: max_results,
-        expand: "changelog"
-      }
-      jira_issues = @client.Issue.jql("", query_options)
+    max_results = 50
+    jira_projects = @client.Project.all
 
-      break if jira_issues.length.zero?
-
-      jira_issues.each do |jira_issue|
-        status = {name: jira_issue.status.name, id: jira_issue.status.id}
-        project = {
-          user_issue_id: "#{@user.id}_#{jira_issue.id}",
-          project_id: jira_issue.project.id, issue_id: jira_issue.id,
-          summary: jira_issue.summary, user_id: @user.id, key: jira_issue.key,
-          status: status, issue_type: jira_issue.fields["issuetype"],
-          epic_link: jira_issue.try(:customfield_10014), epic_name: jira_issue.try(:customfield_10011),
-          due_date: jira_issue.try(:duedate), change_log: jira_issue.try(:changelog),
-          created: jira_issue.try(:created), time_to_close_in_days: time_to_close_in_days(jira_issue),
-          status_transitions: status_transitions(jira_issue)
+    jira_projects.each do |jira_project|
+      loop do
+        query_options = {
+          start_at: start_at,
+          max_results: max_results,
+          expand: "changelog"
         }
-        Issue.upsert(project, unique_by: :user_issue_id)
+        
+        jira_issues = @client.Issue.jql('PROJECT = "' + jira_project.name + '" ', query_options)
+        puts "LOAD  #{jira_project.name}************************************"
+        break if jira_issues.length.zero?
+  
+        jira_issues.each do |jira_issue|
+          puts "UPSERT!!! ************************************"
+          
+  
+          status = {name: jira_issue.status.name, id: jira_issue.status.id}
+          project = {
+            user_issue_id: "#{@user.id}_#{jira_issue.id}",
+            project_id: jira_issue.project.id, issue_id: jira_issue.id,
+            summary: jira_issue.summary, user_id: @user.id, key: jira_issue.key,
+            status: status, issue_type: jira_issue.fields["issuetype"],
+            epic_link: jira_issue.try(:customfield_10014), epic_name: jira_issue.try(:customfield_10011),
+            due_date: jira_issue.try(:duedate), change_log: jira_issue.try(:changelog),
+            created: jira_issue.try(:created), time_to_close_in_days: time_to_close_in_days(jira_issue),
+            status_transitions: status_transitions(jira_issue)
+          }
+          Issue.upsert(project, unique_by: :user_issue_id)
+        end
+  
+        start_at += max_results
       end
 
-      start_at += max_results
     end
+
+    
   end
 
   private
